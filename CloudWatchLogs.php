5c0e0abb24b44b63f241387dccbf6247d11b0e2e
TSURU <tsuyoshi.tsurushima@gmail.com>
2020-10-29 14:09:42 +0900

    UPDATE ログページデザインを修正

--- CloudWatchLogs.php
+++ CloudWatchLogs.php
@@ -37,6 +37,7 @@ class CloudWatchLogs extends SC_Plugin_Base
         parent::register($objHelperPlugin, $priority);
 
         $objHelperPlugin->addAction('LC_Page_Admin_System_Log_Ex_action_before', array($this, 'LC_Page_Admin_System_Log_action_before'), $priority);
+        $objHelperPlugin->addAction('prefilterTransform', array($this, 'prefilterTransform'), $priority);
     }
 
     public function LC_Page_Admin_System_Log_action_before(LC_Page_Admin_System_Log $objPage)
@@ -45,5 +46,28 @@ class CloudWatchLogs extends SC_Plugin_Base
         $objPage->init();
         $objPage->process();
         exit();
+    }
+
+    /**
+     * プレフィルタコールバック関数
+     *
+     * @param string &$source テンプレートのHTMLソース
+     * @param LC_Page_Ex $objPage ページオブジェクト
+     * @param string $filename テンプレートのファイル名
+     * @return void
+     */
+    public function prefilterTransform(&$source, LC_Page_Ex $objPage, $filename)
+    {
+        if (defined('ADMIN_FUNCTION') && ADMIN_FUNCTION) {
+            $deviceType = DEVICE_TYPE_ADMIN;
+        } else {
+            $deviceType = SC_Display_Ex::detectDevice();
+        }
+
+        if ($deviceType === DEVICE_TYPE_ADMIN) {
+            if (strpos($filename, 'system/log.tpl') !== false) {
+                $source = str_replace('<table class="list log">', '<table class="list log">' . "\n" . '<col width="15%"><col width="25%"><col width="60%">', $source);
+            }
+        }
     }
 }
