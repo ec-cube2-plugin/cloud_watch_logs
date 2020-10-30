99f0126d195c63db7d4cb69d1c3077fb9e3feeb1
TSURU <tsuyoshi.tsurushima@gmail.com>
2020-10-30 16:58:51 +0900

    1.2.2

--- plugin_info.php
+++ plugin_info.php
@@ -15,7 +15,7 @@ class plugin_info
     static $CLASS_NAME = "CloudWatchLogs";
 
     /** プラグインバージョン(必須)：プラグインのバージョン. */
-    static $PLUGIN_VERSION = "1.2.1";
+    static $PLUGIN_VERSION = "1.2.2";
 
     /** 対応バージョン(必須)：対応するEC-CUBEバージョン. */
     static $COMPLIANT_VERSION = "2.13.5";
